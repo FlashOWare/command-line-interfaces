d23e1928e8ff242773b0d7a9477f5818b22ce265
Flash0ver <38893694+Flash0ver@users.noreply.github.com>
2026-08-11 11:42:12 +0200

    chore: add Visual Basic sample app

---
+++ source/examples/FlashOWare.CommandLine.Example.VisualBasic/Properties/GlobalSuppressions.vb
@@ -0,0 +1,5 @@
+' Global Suppressions
+
+Imports System.Diagnostics.CodeAnalysis
+
+<Assembly: SuppressMessage("Performance", "CA1861:Avoid constant arrays as arguments", Justification:="Demo App.", Scope:="member", Target:="~M:FlashOWare.CommandLine.Example.Program.Main(System.String[])~System.Int32")>
