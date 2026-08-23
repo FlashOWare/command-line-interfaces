bf206461250df5c5db16a91f8f6582db4aee8194
Flash0ver <38893694+Flash0ver@users.noreply.github.com>
2026-08-23 19:50:09 +0200

    chore: remove redundant comment

--- source/examples/FlashOWare.CommandLine.Example.VisualBasic/Properties/GlobalSuppressions.vb
+++ source/examples/FlashOWare.CommandLine.Example.VisualBasic/Properties/GlobalSuppressions.vb
@@ -1,5 +1,3 @@
-' Global Suppressions
-
 Imports System.Diagnostics.CodeAnalysis
 
 <Assembly: SuppressMessage("Performance", "CA1861:Avoid constant arrays as arguments", Justification:="Demo App.", Scope:="member", Target:="~M:FlashOWare.CommandLine.Example.Program.Main(System.String[])~System.Int32")>
